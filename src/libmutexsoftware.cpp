7aee44ecdd3875ee9568f863d1e23c92e4c26ec7
Rodrigo Bervig <rodrigo.bervig@quintoandar.com.br>
2025-04-29 19:13:36 -0300

    Remove usage of atomic

--- src/libmutexsoftware.cpp
+++ src/libmutexsoftware.cpp
@@ -1,46 +1,46 @@
 #include "libmutexsoftware.h"
-#include <atomic>
+
 #include <algorithm>
 #include <utility>
 
 namespace {
     const int MAX_THREADS = 10;
-    std::atomic<bool> choosing[MAX_THREADS];
-    std::atomic<int> ticket[MAX_THREADS];
+    bool choosing[MAX_THREADS];
+    int ticket[MAX_THREADS];
 }
 
 int get_max_ticket() {
     int max = 0;
     for (int i = 0; i < MAX_THREADS; ++i) {
-        int t = ticket[i].load(std::memory_order_relaxed);
+        int t = ticket[i];
         if (t > max) max = t;
     }
     return max;
 }
 
 void lamport_mutex_lock(int thread_id) {
-    choosing[thread_id].store(true, std::memory_order_relaxed);
+    choosing[thread_id] = true;
 
     int my_ticket = get_max_ticket() + 1;
-    ticket[thread_id].store(my_ticket, std::memory_order_release);
+    ticket[thread_id] = my_ticket;
 
-    choosing[thread_id].store(false, std::memory_order_release);
+    choosing[thread_id] = false;
 
     for (int j = 0; j < MAX_THREADS; ++j) {
         if (j == thread_id) continue;
 
-        while (choosing[j].load(std::memory_order_acquire)) {
+        while (choosing[j]) {
             // busy wait
         }
 
-        while (ticket[j].load(std::memory_order_acquire) != 0 &&
-               std::make_pair(ticket[j].load(), j) <
-               std::make_pair(ticket[thread_id].load(), thread_id)) {
+        while (ticket[j] != 0 &&
+               std::make_pair(ticket[j], j) <
+               std::make_pair(ticket[thread_id], thread_id)) {
             // busy wait
         }
     }
 }
 
 void lamport_mutex_unlock(int thread_id) {
-    ticket[thread_id].store(0, std::memory_order_release);
+    ticket[thread_id] = 0;
 }
